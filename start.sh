03cb496d449c3c7e7a3975d62063a745599a1ebb
Zen-44 <92331357+Zen-44@users.noreply.github.com>
2024-05-05 16:38:21 +0300

    small changes

--- start.sh
+++ start.sh
@@ -4,10 +4,12 @@ pkill gunicorn
 screen -X -S bot quit
 screen -X -S site quit
 
-cd discord-bot
+cd idena-auth
 screen -dmS bot python3.11 bot.py
-screen -dmS site gunicorn -b :443 --certfile=/root/discord-bot/certs/idena.cloud.crt --keyfile=/root/discord-bot/certs/idena.cloud.key auth:app
+screen -dmS site gunicorn -b :443 --certfile=/root/idena-auth/certs/idena.cloud.crt --keyfile=/root/idena-auth/certs/idena.cloud.key auth:app
 cd
 
 # crontab for reboots
-# (crontab -l 2>/dev/null; echo "@reboot bash $PWD/discord-bot/start.sh") | crontab -
+# (crontab -l 2>/dev/null; echo "@reboot bash $PWD/idena-auth/start.sh") | crontab -
+# magic symbol remover
+# sed -i 's/\r$//' idena-auth/start.sh
